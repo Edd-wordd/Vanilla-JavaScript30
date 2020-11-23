47f4dfb8e08e96751f5433bf9459c1aa57ac06c1
Edd-wordd <edward.plasencio@gmail.com>
2020-11-22 22:00:54 -0800

    add styling and functionality

---
+++ 01-JavaScript Drumkit/script.js
@@ -0,0 +1,18 @@
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return; //if no key has a keycode return nothing
+
+  key.classList.add("playing");
+  audio.currentTime = 0; //rewinds the sound
+  audio.play(); // plays the wav sound
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
