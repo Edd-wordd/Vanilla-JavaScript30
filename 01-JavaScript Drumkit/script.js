574f1a761ab28804cdbb3058f37c4146477f4fe8
Edd-wordd <edward.plasencio@gmail.com>
2020-11-22 22:06:56 -0800

    apply prettier, add comments

--- 01-JavaScript Drumkit/script.js
+++ 01-JavaScript Drumkit/script.js
@@ -1,18 +1,19 @@
 function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  e.target.classList.remove("playing");
+   if (e.propertyName !== "transform") return;
+   e.target.classList.remove("playing");
 }
 
 function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  if (!audio) return; //if no key has a keycode return nothing
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+   if (!audio) return; //if no key has a keycode return nothing
 
-  key.classList.add("playing");
-  audio.currentTime = 0; //rewinds the sound
-  audio.play(); // plays the wav sound
+   key.classList.add("playing");
+   audio.currentTime = 0; //rewinds the sound
+   audio.play(); // plays the wav sound
 }
 
 const keys = Array.from(document.querySelectorAll(".key"));
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+//when you press down on a key it triggers the playsound fuunction
 window.addEventListener("keydown", playSound);
